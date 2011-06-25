0d99a048e8e6aab4f5f8e0b085674579619efc7b
gmsh <gmshcn@gmail.com>
2011-06-25 15:46:55 +0800

    add dc_set(domain charset) header & source files.

---
+++ dl_cache/dc_set.c
@@ -0,0 +1,18 @@
+/*
+ * dc_set.c
+ *
+ *  Created on: Jun 25, 2011
+ *      Author: gmsh
+ */
+
+
+#include "dc_set.h"
+
+uint32 get_code(uint8 character)
+{
+	return 0;/*TODO*/
+}
+uint8 get_char(uint32 code)
+{
+	return 0;/*TODO*/
+}
