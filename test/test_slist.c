32fac60566c6736918bfe0fef3f97cdaf281f07b
Junwei Wang <wakemecn@gmail.com>
2011-06-25 22:20:06 +0800

    new file:   test_slist.c 	modified:   ../dc_mm/dc_mm.c 	modified:   ../dc_mm/mm_struct.h 	modified:   ../include/dc_mm.h

---
+++ test/test_slist.c
@@ -0,0 +1,12 @@
+/*
+ * filename:	test_slist.c
+ * author:		wakemecn
+ * description: the test program for slist created by gmsh
+ * date:		Jun 25,2009
+ */
+
+#include "slist.h"
+
+int main(){
+		
+}
