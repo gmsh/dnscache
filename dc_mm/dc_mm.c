48564efd297513cd66a2c454bb278ea4101e7db8
Junwei Wang <wakemecn@gmail.com>
2011-06-26 12:44:57 +0800

    modified:   dc_mm/dc_mm.c 	modified:   dc_mm/mm_structs.h

--- dc_mm/dc_mm.c
+++ dc_mm/dc_mm.c
@@ -12,7 +12,7 @@ struct slist * mm_pre_alloc(uint32 capacity, uint16 counts){
 	struct slist * init_idle_list;
 	void * chunck_ptr;
 	//todo
-	init_idle_list=mk_slist(malloc(0),num_each_chunks[capacity-SMALL]);
+	init_idle_list=mk_slist(malloc(0),counts);
 	while(counts--){
 		chunk_ptr=malloc(POW2(capacity));
 		push(chunk_ptr,init_idle_list);
@@ -35,8 +35,28 @@ int mm_init(){
 }
 
 void * dc_alloc(size_t size){
-	
-	  return malloc(size);
+	uint32 cap=i(uint32)LOG2(size)+1; /* */
+	struct slist * extra_list;
+	int position=cap-SMALL;  /* position of the pointer in the array. */
+	if( cap < SMALL || cap>BIG){
+		if(elm_table[cap]==null){
+			elm_table[cap]=chunks_list=mm_pre_alloc(cap,DEFAULT_EXTRA);
+			return select_extra(cap,elm_table[cap]);
+		}
+		if(elm_table[cap]->idle_num==0)
+			elm_table[cap]->chunks_list=sl_expand(elm_table[cap]->chunks_list,malloc(0),DEFAULT_EXTRA);
+		return select_extra(cap,elm_table[cap]);
+
+	}
+
+
+
+		|| chunk_manager_table[position]->idle_num==0){
+		extra_list=mm_pre_alloc(cap,num_extra_chunks[position]);
+		chunk_manager_table[positon]->extra_chunks=extra_list;
+	}
+	if()
+	return ;
 }
 
 void dc_free(void * ptr)
--- dc_mm/mm_structs.h
+++ dc_mm/mm_structs.h
@@ -13,12 +13,12 @@
 #define CHUNCK_TYPE_NUM ( BIG - SMALL + 1 )	/* counts of  chunck's type */
 #define POW2(x) pow(2,x)
 #define	LOG2(x) (log(x)/log(2))
+#define DEFAULT_EXTRA 4;
 
 #include <math.h>
 #include "typedefs.h"
 
-//typedef 
-struct chunck_manager {
+struct chunck_manager {	/* manage the chunks with small capacity. */
 	uint32	chunck_cap;	/* the defined n for the capacity of  2^n-bytes chunks */
 	uint16	idle_num;	/*  the count of pre-alllocated chunks */
 	slist * idle_chucks;	/* the list of idle chunk or chunks */
@@ -26,9 +26,19 @@ struct chunck_manager {
 	slist * extra_chunks;   /* the dynamicly allocated chunks list */
 }	chunk_manager_table[CHUNCK_TYPE_NUM];
 
+struct extra_list_manager{	/* manager the extra list of chunks */
+	uint16 idle_num;	/* idle number */
+	slist * chunks_list;/* pointer to the list */
+}	elm_table[30];/* as the name shows that num in []  presents the cap*/
+
 uint16 num_each_chunks[]={ 64, 64, 64, 64, 64,
                            32, 32, 32, 32, 32,
 			   16, 16,  8 }
+			   /* number of each type of chunks */
+uint16 num_extra_chunks[]={  8,  8,  8,  8,  8,
+                            4,  4,  4,  4,  4,
+			    2,  2,  2 }
+				/* number of each type of extra chunks */
 
 stuct slist idle_chunks_table[CHUNK_TYPE_NUM];  /* array of pointers to
                                                  * idle chunks lists.
