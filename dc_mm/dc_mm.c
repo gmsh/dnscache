f060db70fdbf016481e66b8e152aa01a381abf1c
Junwei Wang <wakemecn@gmail.com>
2011-06-26 09:07:07 +0800

    modified:   ../dc_mm/dc_mm.c 	modified:   ../dc_mm/mm_structs.h 	modified:   Makefile

--- dc_mm/dc_mm.c
+++ dc_mm/dc_mm.c
@@ -11,8 +11,10 @@
 struct slist * mm_pre_alloc(uint32 capacity, uint16 counts){
 	struct slist * init_idle_list;
 	void * chunck_ptr;
+	//todo
+	init_idle_list=mk_slist(malloc(0),num_each_chunks[capacity-SMALL]);
 	while(counts--){
-		chunk_ptr=malloc(pow(2,capacity));
+		chunk_ptr=malloc(POW2(capacity));
 		push(chunk_ptr,init_idle_list);
 	}
 	return init_idle_list;	
@@ -21,14 +23,20 @@ struct slist * mm_pre_alloc(uint32 capacity, uint16 counts){
 int mm_init(){
 	uint32 i;
 	while((i=CHUNK_TYPE_NUM)--){
-		idle_chunks[i]=mm_pre_alloc((SMALL+i), num_each_chunks[i]);	
+		idle_chunks_table[i]=mm_pre_alloc((SMALL+i), num_each_chunks[i]);	
+		chunk_manager_table[i]=(struct chunk_manager *)malloc(sizeof(chunk_manager));
+		chunk_manager_table[i]->chunk_cap=SMALL+i;
+		chunk_manager_table[i]->idle_num=num_each_chunks[i];
+		chunk_manager_table[i]->idle_chunks=idle_chunks_table[i];
+		chunk_manager_table[i]->alloced_chunks=null;
+		chunk_manager_table[i]->extra_chunks=null;
 	}
 	return 0;	
 }
 
-void * dc_alloc(size_t size)
-{
-  return malloc(size);
+void * dc_alloc(size_t size){
+	
+	  return malloc(size);
 }
 
 void dc_free(void * ptr)
--- dc_mm/mm_structs.h
+++ dc_mm/mm_structs.h
@@ -11,6 +11,8 @@
 #define SMALL 8			/* the 2^SMALL is the smallese chunks */
 #define	BIG	20			/* the 2^BIG is the biggest chunks */
 #define CHUNCK_TYPE_NUM ( BIG - SMALL + 1 )	/* counts of  chunck's type */
+#define POW2(x) pow(2,x)
+#define	LOG2(x) (log(x)/log(2))
 
 #include <math.h>
 #include "typedefs.h"
@@ -18,16 +20,22 @@
 //typedef 
 struct chunck_manager {
 	uint32	chunck_cap;			/* the defined n for the capacity of  2^n-bytes chunks */
-	uint16	chunck_total_num;	/*  the count of pre-alllocated chunks */
-	slist * idel_chucks;		/* the list of idle chunk or chunks */
+	uint16	idle_num;	/*  the count of pre-alllocated chunks */
+	slist * idle_chucks;		/* the list of idle chunk or chunks */
 	slist * alloced_chucks;		/* the list of allocated chunk or chunks */
+	slist * extra_chunks;       /* the dynamicly allocated chunks list */
 }	chunk_manager_table[CHUNCK_TYPE_NUM];
 
 uint16 num_each_chunks[]={ 64, 64, 64, 64, 64,
                            32, 32, 32, 32, 32,
 						   16, 16,  8 }
 
-stuct slist idle_chunks[CHUNK_TYPE_NUM];
-stuct slist used_chunks[CHUNK_TUPE_NUM]; 
+stuct slist idle_chunks_table[CHUNK_TYPE_NUM];  /* array of pointers to
+                                                 * idle chunks lists.
+												 */
+stuct slist alloced_chunks_table[CHUNK_TUPE_NUM]; /* array of pointers 
+                                                   * to allocated chunks
+												   * lists.
+												   */
 
 #endif
