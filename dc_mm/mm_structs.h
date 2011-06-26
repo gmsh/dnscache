a4193e6145ec4d39a659e18d6ef9ed3d5d5ae266
wakemecn <wakemecn@gmail.com>
2011-06-25 18:10:12 -0700

    Edited dc_mm/mm_structs.h via GitHub

--- dc_mm/mm_structs.h
+++ dc_mm/mm_structs.h
@@ -1,15 +1,15 @@
 /*
  * filename:	mm_struct.c
- * author:		wakemecn
- * date:		6/23/2011
+ * author:	wakemecn
+ * date:	6/23/2011
  * description: define structs used for memory management.
  */
 
 #ifndef _MM_STUCT_H
 #define _MM_STUCT_H
 
-#define SMALL 8			/* the 2^SMALL is the smallese chunks */
-#define	BIG	20			/* the 2^BIG is the biggest chunks */
+#define SMALL 8			                /* the 2^SMALL is the smallese chunks */
+#define	BIG	20			        /* the 2^BIG is the biggest chunks */
 #define CHUNCK_TYPE_NUM ( BIG - SMALL + 1 )	/* counts of  chunck's type */
 #define POW2(x) pow(2,x)
 #define	LOG2(x) (log(x)/log(2))
@@ -19,23 +19,23 @@
 
 //typedef 
 struct chunck_manager {
-	uint32	chunck_cap;			/* the defined n for the capacity of  2^n-bytes chunks */
+	uint32	chunck_cap;	/* the defined n for the capacity of  2^n-bytes chunks */
 	uint16	idle_num;	/*  the count of pre-alllocated chunks */
-	slist * idle_chucks;		/* the list of idle chunk or chunks */
-	slist * alloced_chucks;		/* the list of allocated chunk or chunks */
-	slist * extra_chunks;       /* the dynamicly allocated chunks list */
+	slist * idle_chucks;	/* the list of idle chunk or chunks */
+	slist * alloced_chucks;	/* the list of allocated chunk or chunks */
+	slist * extra_chunks;   /* the dynamicly allocated chunks list */
 }	chunk_manager_table[CHUNCK_TYPE_NUM];
 
 uint16 num_each_chunks[]={ 64, 64, 64, 64, 64,
                            32, 32, 32, 32, 32,
-						   16, 16,  8 }
+			   16, 16,  8 }
 
 stuct slist idle_chunks_table[CHUNK_TYPE_NUM];  /* array of pointers to
                                                  * idle chunks lists.
-												 */
+						 */
 stuct slist alloced_chunks_table[CHUNK_TUPE_NUM]; /* array of pointers 
                                                    * to allocated chunks
-												   * lists.
-												   */
+						   * lists.
+						   */
 
 #endif
