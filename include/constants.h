20542163b03cbd86c18befe98e78884bc415b0fb
gmsh <gmshcn@gmail.com>
2011-06-26 20:35:42 +0800

    add constants.h

---
+++ include/constants.h
@@ -0,0 +1,13 @@
+/*
+ * constants.h
+ *
+ *  Created on: Jun 26, 2011
+ *      Author: gmsh
+ */
+
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+#define INITIAL_DATRIE_TAIL_POOL_SIZE 512
+
+#endif /* CONSTANTS_H_ */
