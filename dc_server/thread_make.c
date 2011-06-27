bbbcbb276d16e7c6478cb083a3d16d9933632dba
Synckey Wang <synckey@gmail.com>
2011-06-27 10:28:55 +0800

    Finish and test the dns thread 	modified:   dc_server/Makefile 	new file:   dc_server/libwraps.a 	modified:   dc_server/server.h 	new file:   dc_server/server.h.gch 	modified:   dc_server/test.c 	new file:   dc_server/testclient.c 	modified:   dc_server/thread_make.c

--- dc_server/thread_make.c
+++ dc_server/thread_make.c
@@ -12,63 +12,64 @@
 
 void thread_make_dns(int i)
 {
-	void	*thread_main_dns(void *);
-	if(pthread_create(&dns_thread_tptr[i].thread.tid,
-		NULL, &thread_main_dns, (void *)i) != 0){
-		printf("can not create dns thread %d\n", i);
-		exit(1);
-	}
+	void*	thread_main_dns(void *);
+	Pthread_create(&dns_thread_tptr[i].thread_tid,
+		NULL, &thread_main_dns, (void *)i);
 	return;
-
-	
-
 }
 
 
 void *
 thread_main_dns(void *arg)
 {
-	int dnsrequest(const char *name ,uint32 *ipaddr);
-	
-	printf("thread %d starting \n",(int)arg );
+	int dnsrequest(const char *name, uint32 *ipaddr);
+	int connfd, *number, count, total;
+	char *domain ;
+	uint32 *ipptr;
+	pthread_mutex_t *mutex;
+	printf("thread %d starting \n", (int)arg );
+
 	for(;;){
-		if(Pthread_mutex_lock(&dns_array_mutex) !=0 ){
-			printf("can not get lock\n");
-			exit(1);
-		}
-		while(iget == iput){
-			if(pthread_cond_wait(&dns_array_cond,
-					&dns_array_mutex) != 0){
-				printf("can not wait \n");
-				exit(1);			
-			}
-		}
-		dnsrequest(dns_array[iget].domain,dns_array[iget].ipptr
-			+dns_array[iget]+count);
-		free(domain);			//
-		if(pthread_mutex_lock(dns_array[iget].mutex) !=0 ){
-			printf("can not get lock\n");
-			exit(1);
-		}
-		if(--dns_array[iget].number == 0){
-			int connfd = sockfd;
-			write(connfd, ipptr, 
-				dns_array[iget].total*sizeof(uint32));
-			close(connfd);
-			free(ipptr);
 
-		}
-		if(pthread_mutex_unlock(dns_array[iget].mutex) !=0 ){
-			printf("can not unlock\n");
-			exit(1);
-		}
-		if(++iget == MAXTHREADS)
+		Pthread_mutex_lock(&dns_array_mutex);
+
+		while(iget == iput)
+			Pthread_cond_wait(&dns_array_cond,
+					&dns_array_mutex);
+		
+		printf("thread %d  working \n", pthread_self());
+
+		connfd	= dns_array[iget].sockfd;
+		domain  = dns_array[iget].domain;
+		number	= dns_array[iget].number;
+ 		count	= dns_array[iget].count;
+		total	= dns_array[iget].total;
+		ipptr	= dns_array[iget].ipptr;
+		mutex	= dns_array[iget].mutex;
+		
+		if(++iget == MAXDNSTHREADS)
 			iget = 0;		
-		if(pthread_mutex_unlock(&dns_array_mutex) !=0 ){
-			printf("can not unlock\n");
-			exit(1);
-		}
 
+		Pthread_mutex_unlock(&dns_array_mutex);
+	
+
+
+		
+		//do our job 
+		dnsrequest(domain, ipptr + count);
+		free(domain);	//free the memory of domain
+	
+		Pthread_mutex_lock(mutex);
+		(*number)--;
+		Pthread_mutex_unlock(mutex);
+
+		if((*number) == 0){
+			write(connfd, ipptr, total*sizeof(uint32));
+		//	close(connfd);
+			free(ipptr);
+			free(number);
+			free(mutex);
+		} 
 
 
 	}
