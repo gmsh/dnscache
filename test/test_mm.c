2936489d991da2836c36e4a1c10da7c20200a557
Junwei Wang <wakemecn@gmail.com>
2011-06-30 12:32:13 +0800

    modified:   test_mm.c

--- test/test_mm.c
+++ test/test_mm.c
@@ -2,11 +2,11 @@
 
 main(){
 	mm_init();
-	void * ptr[10];
+	void * ptr[18];
 	int i, j;
-	for(i = 0; i < 10; i++)
-		ptr[i] = dc_alloc(256);
-	for(j = 0; j < 10; j++ )
+	for(i = 0; i < 18; i++)
+		ptr[i] = dc_alloc(1048576);
+	for(j = 0; j < 18; j++ )
 			dc_free(ptr[j]);
 	ptr[i] = dc_alloc(256);
 	dc_free(ptr[i]);
