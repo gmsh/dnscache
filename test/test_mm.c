9fd5ebfd387d3be6f9f61606ed9193b0aabb72b3
wakemecn <wakemecn@gmail.com>
2011-07-01 10:50:39 +0800

    modified:   test/test_mm.c

--- test/test_mm.c
+++ test/test_mm.c
@@ -6,38 +6,39 @@
 main(){
 	mm_init();
 	void * ptr[2000];
-	int i, j;int count=25;
+	int i, j;int count=20;
 while(1){
-//	while(--count < 25 && count > 1){
-	count=10;
-		for(i = 0; i < 40; i++)
+	while(--count < 20 && count > 18){
+		for(i = 0; i < 2; i++)
 			ptr[i] = dc_alloc(POW2(count));
 		print_statics();
-		for(j = 0; j < 40; j++ )
+//	short x=1, y;
+//		while(x++!=0){
+//			y=x * x;
+//		}
+//		y = y * 5;
+//		printf("%d\n",y);
+		for(j = 0; j < 2; j++ )
 			dc_free(ptr[j]);
 		print_statics();
-		for(i = 0; i < 40; i++)
+	}
+
+	while(--count < 20 && count > 18){
+		for(i = 0; i < 2; i++)
 			ptr[i] = dc_alloc(POW2(count));
 		print_statics();
-		for(j = 0; j < 40; j++ )
+//		short x=1, y;
+///		while(x++!=0){
+//			y=x * x;
+//		}
+//		y = y * 5;
+//		printf("%d\n",y);
+		for(j = 0; j < 2; j++ )
 			dc_free(ptr[j]);
 		print_statics();
-//	}
-//	while(--count < 25 && count > 1){
-		for(i = 0; i < 40; i++)
-			ptr[i] = dc_alloc(POW2(count));
-		print_statics();
-		for(j = 0; j < 40; j++ )
-			dc_free(ptr[j]);
-		print_statics();
-		for(i = 0; i < 40; i++)
-			ptr[i] = dc_alloc(POW2(count));
-		print_statics();
-		for(j = 0; j < 40; j++ )
-			dc_free(ptr[j]);
-		print_statics();
-//		wait();
-//	}
+	}
+
 	print_statics();
+	count = 20;
 }
 }
